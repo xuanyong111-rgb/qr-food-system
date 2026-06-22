0b34d5dd5a26792f9d7294b054cba8dc374adbb7
汶轩 <xuanyong111@gmail.com>
2026-06-23 02:32:28 +0800

    feat: 完善座位显示、按钮对齐与云端实时死锁防御机制

---
+++ menus/customer_shop_B.js
@@ -0,0 +1,108 @@
+window.currentShopMenu = [ 
+            {
+                categoryTitle: "主食",
+                items: [ 
+                    { name: "A1 义大利面", price: "RM 4.50", desc: "纯正法国黄油", imgUrl: "images/义大利面.png",
+                        options: [
+                            { label: "DE1. 椰糖 Gula Melaka", extraPrice: 1.10 },
+                            { label: "DE1. 红糖红糖", extraPrice: 0 },
+                            { label: "DE1. 红糖+椰糖混合糖", extraPrice: 1.50 }
+                            
+                        ],
+                        options2: [
+                            { label: "加煎蛋", extraPrice: 1.50 },
+                            { label: "不加蛋", extraPrice: 0 }
+                        ]
+                    },
+                    {
+                    name: "A2炸鸡扒饭", price: "RM 8.00", desc: "高级宇治抹茶", imgUrl: "images/炸鸡扒饭.png",
+                        options: [
+                            { label: "DE1. 椰糖 Gula Melaka", extraPrice: 1.10 },
+                            { label: "DE1. 红糖红糖", extraPrice: 0 },
+                            { label: "DE1. 红糖+椰糖混合糖", extraPrice: 1.50 }
+                        ],
+                        options2: [
+                            { label: "加煎蛋", extraPrice: 1.50 },
+                            { label: "不加蛋", extraPrice: 0 }
+                        ]
+                    }
+                ]
+            }, 
+            {
+                categoryTitle: "面包 ",
+                items: [
+                    {name: "A3法式原味牛角包", price: "RM 4.50", desc: "纯正法国黄油", imgUrl: "images/法式原味牛角包.png" ,
+                        options: [
+                            { label: "DE1. 椰糖 Gula Melaka", extraPrice: 1.10 },
+                            { label: "DE1. 红糖红糖", extraPrice: 0 },
+                            { label: "DE1. 红糖+椰糖混合糖", extraPrice: 1.50 }
+                        ],
+                        options2: [
+                            { label: "加煎蛋", extraPrice: 1.50 },
+                            { label: "不加蛋", extraPrice: 0 }
+                        ]
+                    },
+                    {
+                    name: "A4日式宇治抹茶生吐司", price: "RM 8.00", desc: "高级宇治抹茶", imgUrl: "images/日式宇治抹茶生吐司.png",
+                        options: [
+                            { label: "DE1. 椰糖 Gula Melaka", extraPrice: 1.10 },
+                            { label: "DE1. 红糖红糖", extraPrice: 0 },
+                            { label: "DE1. 红糖+椰糖混合糖", extraPrice: 1.50 }
+                        ],
+                        options2: [
+                            { label: "加煎蛋", extraPrice: 1.50 },
+                            { label: "不加蛋", extraPrice: 0 }
+                        ]
+                    }
+                ]
+            },
+            {
+                categoryTitle: "甜品 ",
+                items: [
+                    { name: "A5法式焦糖布丁", price: "RM 5.00", desc: "口感细腻，焦糖微苦", imgUrl: "images/法式焦糖布丁.png",
+                        options: [
+                            { label: "DE1. 椰糖 Gula Melaka", extraPrice: 1.10 },
+                            { label: "DE1. 红糖红糖", extraPrice: 0 },
+                            { label: "DE1. 红糖+椰糖混合糖", extraPrice: 1.50 }
+                        ],
+                        options2: [
+                            { label: "加煎蛋", extraPrice: 1.50 },
+                            { label: "不加蛋", extraPrice: 0 }
+                        ]
+                        
+                    },
+                    {
+                    name: "A6纽约重芝士蛋糕", price: "RM 6.50", desc: "浓郁奶香，入口即化", imgUrl: "images/纽约重芝士蛋糕.png", 
+                        options: [
+                            { label: "DE1. 椰糖 Gula Melaka", extraPrice: 0 },
+                            { label: "DE1. 红糖红糖", extraPrice: 0 },
+                            { label: "DE1. 红糖+椰糖混合糖", extraPrice: 1.50 }
+                        ],
+                        options2: [
+                            { label: "加煎蛋", extraPrice: 1.50 },
+                            { label: "不加蛋", extraPrice: 0 }
+                        ]
+                    }
+                ]
+            },
+            {
+                categoryTitle: "夏日冰沙 & 汽水 ",
+                items: [
+                    { name: "A7芒果椰椰冰沙", price: "RM 6.00", desc: "新鲜芒果搭配清甜椰乳", imgUrl: "images/customer_shop_B/芒果椰椰冰沙.png",
+                        options: [
+                            { label: "DE1. 椰糖 Gula Melaka", extraPrice: 1.10 },
+                            { label: "DE1. 红糖红糖", extraPrice: 1.80 },
+                            { label: "DE1. 红糖+椰糖混合糖", extraPrice: 1.50 }
+                        ]
+                    },
+                    {
+                    name: "A8百香果苏打汽水", price: "RM 4.50", desc: "酸甜解腻，气泡充沛", imgUrl: "images/customer_shop_B/百香果苏打汽水.png",
+                        options: [
+                            { label: "DE1. 椰糖 Gula Melaka", extraPrice: 1.10 },
+                            { label: "DE1. 红糖红糖", extraPrice: 1.80 },
+                            { label: "DE1. 红糖+椰糖混合糖", extraPrice: 1.50 }
+                        ]
+                    }
+                ]
+            }// 💡 以后还想加“糖水”、“小吃”，直接按照上面的格式在下面复制逗号，继续往下加就行！
+        ];
---
+++ shops.js
@@ -0,0 +1,19 @@
+const allShopsData = {
+    "wuwangxin_A🌟": {
+        shopName: "勿忘心 🌟",
+        logoUrl: "images/WuWangXin_A🌟/勿忘心.png", 
+        slogan: "每日现烤 · 纯正手作 · 幸福滋味",
+        accounts: {
+            "wenxuan": "7140a664e3d57398467ece5a6fc210106f1bdd706140baddc181c2b15da67710"
+        }
+    },
+    "customer_shop_B": {
+        shopName: "新顾客的专属餐饮店 ☕", 
+        logoUrl: "images/customer_shop_B/client_logo.png", 
+        slogan: "新鲜冲泡 · 香气四溢",
+        accounts: {
+            "b" : "fd4d18552b5be40e9d2dd7615973c8ffe9aac393666efab859748c268ed5e49d"
+        }
+    }
+};
+        
