335f75a95aedbfa0103b89f5a803057fef44c8d1
汶轩 <xuanyong111@gmail.com>
2026-06-24 20:05:58 +0800

    fix: 修复收银台与后厨 API 请求漏传 auth 密匙导致的 401 拦截及 #rror 报错

--- shops.js
+++ shops.js
@@ -3,6 +3,7 @@ const allShopsData = {
         shopName: "勿忘心 🌟",
         logoUrl: "images/wuwangxin_a/勿忘心.png", 
         slogan: "每日现烤 · 纯正手作 · 幸福滋味",
+        maxDevices: 2,
         accounts: {
             "wenxuan": "7140a664e3d57398467ece5a6fc210106f1bdd706140baddc181c2b15da67710"
         }
@@ -11,6 +12,7 @@ const allShopsData = {
         shopName: "新顾客的专属餐饮店 ☕", 
         logoUrl: "images/customer_shop_B/client_logo.png", 
         slogan: "新鲜冲泡 · 香气四溢",
+        maxDevices: 2,
         accounts: {
             "b" : "fd4d18552b5be40e9d2dd7615973c8ffe9aac393666efab859748c268ed5e49d"
         }
